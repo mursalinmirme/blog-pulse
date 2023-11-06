9f2869d8c9fec32eb0b085da32ff12f2ed84bd79
mursalinmirme <mursalinmir02@gmail.com>
2023-11-06 22:04:59 +0600

    I have make blog uploading system in database and when successfully posted a blog user will see a successfully also make a 404 page

--- src/pages/AddBlog.jsx
+++ src/pages/AddBlog.jsx
@@ -1,38 +1,71 @@
 import { Button } from "@mui/material";
-import { Link } from "react-router-dom";
-import { FcGoogle } from 'react-icons/fc';
+import useAuth from "../hooks/useAuth";
+import axios from "axios";
+import toast from "react-hot-toast";
 const AddBlog = () => {
+    const { user } = useAuth();
+    console.log(user?.uid);
+    const handleAddBlog = (e) => {
+        e.preventDefault();
+        const date = new Date();
+        const form = e.target;
+        const blogTitle = form.blogTitle.value;
+        const blogImage = form.blogImage.value;
+        const category = form.category.value;
+        const shortDescription = form.shortDescription.value;
+        const longDescription = form.longDescription.value;
+        const bloggerId = user?.uid;
+        const bloggerName = user?.displayName;
+        const bloggerEmail = user?.email;
+        const blogPostTime = date.toISOString();
+        const newBlog = {blogTitle, blogImage, category, shortDescription, longDescription, bloggerId, bloggerName, bloggerEmail, blogPostTime};
+        console.log(newBlog);
+        const toastId = toast.loading('Posting...')
+        axios.post('http://localhost:5000/addnewblog', newBlog)
+        .then(res => {
+            console.log(res.data);
+            if(res.data.acknowledged){
+                toast.success("Blog post successfully", { id : toastId });
+                form.reset();
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            toast.success(err.message, { id : toastId });
+        })
+    }
     return (
         <div className="mt-20 max-w-screen-2xl mx-auto">
             <div className="w-1/2 px-10 py-8 mx-auto border shadow rounded-xl">
                 <h3 className="text-center mt-3 text-[#3A5A40] text-2xl font-semibold font-fontNoto">{`Make a New Blog`}</h3>
-                <form className="mt-10" action="">
+                <form onSubmit={handleAddBlog} className="mt-10" action="">
                     <div className="flex flex-col">
                         <label className="mb-1" htmlFor="">Blog title</label>
-                        <input className="border px-3 py-2.5 outline-none" type="text" name="" id="" placeholder="Enter your blog title" />
+                        <input className="border px-3 py-2.5 outline-none" type="text" name="blogTitle" id="" placeholder="Enter your blog title" required />
                     </div>
                     <div className="flex flex-col mt-7">
                         <label className="mb-1" htmlFor="">Blog image URL</label>
-                        <input className="border px-3 py-2.5 outline-none" type="text" name="" id="" placeholder="Enter your blog image URL" />
+                        <input className="border px-3 py-2.5 outline-none" type="text" name="blogImage" id="" placeholder="Enter your blog image URL" required />
                     </div>
                     <div className="flex flex-col mt-7">
                         <label className="mb-1" htmlFor="">Category</label>
-                        <select className="border px-3 py-2.5 outline-none" name="" id="">
-                            <option value="">Travel Destinations</option>       
-                            <option value="">Festivals and Celebrations</option>       
-                            <option value="">Adventure and Exploration</option>       
-                            <option value="">Art and Creativity</option>       
-                            <option value="">Tech Reviews</option>       
-                            <option value="">Technology Trends</option>       
+                        <select className="border px-3 py-2.5 outline-none" name="category" id="" required>
+                            <option value="">Select category</option>       
+                            <option value="travel destinations">Travel Destinations</option>       
+                            <option value="festivals and celebrations">Festivals and Celebrations</option>       
+                            <option value="adventure and exploration">Adventure and Exploration</option>       
+                            <option value="art and creativity">Art and Creativity</option>       
+                            <option value="tech reviews">Tech Reviews</option>       
+                            <option value="technology trends">Technology Trends</option>       
                         </select>
                     </div>
                     <div className="flex flex-col mt-7">
                         <label className="mb-1" htmlFor="">Short Description</label>
-                        <input className="border px-3 py-2.5 outline-none" type="text" name="" id="" placeholder="Enter your blog image URL" />
+                        <input className="border px-3 py-2.5 outline-none" type="text" name="shortDescription" id="" placeholder="Enter your blog image URL" required />
                     </div>
                     <div className="flex flex-col mt-7">
                         <label className="mb-1" htmlFor="">Long Description</label>
-                        <textarea name="longDescription" className="border resize-none h-36 outline-none"></textarea>
+                        <textarea name="longDescription" className="border resize-none p-4 h-36 outline-none" required></textarea>
                     </div>
 
                     <Button type="submit" style={{background: '#588157', color: 'white', fontWeight: '600', marginTop: '30px'}} variant="solid" className="w-full h-12 text-base font-medium font-fontNoto">Post Blog</Button>
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,8 +1,16 @@
-
+import { Button } from "@mui/material";
+import { MdOutlineArrowBack } from 'react-icons/md'
+import { Link } from "react-router-dom";
 const ErrorPage = () => {
     return (
-        <div>
-            this is error page
+        <div className="flex justify-center items-center h-screen">
+            <div className="text-center">
+                <h3 className="text-9xl font-bold text-[#3A5A40]">404</h3>
+                <h4 className="text-4xl mt-6 font-semibold text-[#588157]">Opps.... Page not found</h4>
+                <Link to={'/'}>
+                  <Button style={{marginTop: '45px', background: '#3A5A40', color: '#ffffff'}} variant="sollid" className="flex gap-1 h-11"><MdOutlineArrowBack className="text-2xl"></MdOutlineArrowBack><span>Back to Home</span></Button>
+                </Link>
+            </div>
         </div>
     );
 };
